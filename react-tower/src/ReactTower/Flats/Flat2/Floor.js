ea92fc3de25cae685c8f8d27ecfda6fa87f86635
Yuliia <bodnar.yuliia@gmail.com>
2021-06-20 18:00:18 +0200

    flat 2 , tv, window

--- react-tower/src/ReactTower/Flats/Flat2/Floor.js
+++ react-tower/src/ReactTower/Flats/Flat2/Floor.js
@@ -1,10 +1,12 @@
-import RemoteController from "./mainComponents/RemoteController";
 
 function Floor() {
+  
+
   return (
   <div>
+    
     <div>Julia</div>
-    <div><RemoteController/></div>
+      
   </div>
   );
 }
